6223661a3f7f939a70b4c7c491db9bc2d76e7a41
hudaahmed28 <hudaahmed28@users.noreply.github.com>
2026-02-18 15:13:12 -0500

    Initial commit (Add .gitignore and project files)

---
+++ 01_basics.js
@@ -0,0 +1,4 @@
+//code to push to github
+var a = 10;
+console.log(a);
+console.log("Hello World")
---
+++ Chapter_01_basics/01_basics.js
@@ -0,0 +1,3 @@
+//this is a basic code that i am trying to push it to git
+
+console.log("Hello World");
